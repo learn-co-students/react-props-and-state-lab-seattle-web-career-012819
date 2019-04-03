d4d3135908301cb67faff45e1c33eefde4a1004c
Lucy Suddenly <lucysuddenly@Lucys-MacBook-Air.local>
2019-04-02 17:17:56 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,34 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      ...this.state,
+      filters: {
+        type: event.target.value
+      }
+    })
+  }
+
+  onAdoptPet = (petId) => {
+    let pets = this.state.pets.map(pet => {
+      return pet.id === petId ? { ...pet, isAdopted: true } : pet;
+    })
+    this.setState({pets})
+  }
+
+  onFindPetsClick = () => {
+    let url = "/api/pets"
+
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`
+    }
+
+    fetch(url)
+    .then(resp => resp.json())
+    .then(pets => this.setState({pets}))
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +52,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -3,9 +3,17 @@ import React from 'react'
 import Pet from './Pet'
 
 class PetBrowser extends React.Component {
-  render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+  render(){
+    return <div className="ui cards">{this.props.pets.map(pet => {
+      return <Pet onAdoptPet={this.props.onAdoptPet} pet={pet}/>
+    })}</div>
+
   }
+
+
+
+
+
 }
 
 export default PetBrowser
