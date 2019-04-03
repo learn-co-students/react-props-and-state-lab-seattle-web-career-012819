f6f201fad4a504fda8cfd18504ed5b0a7ee6baaf
github username <crishanks12@gmail.com>
2019-04-02 18:16:56 -0700

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,52 @@ class App extends React.Component {
     }
   }
 
+  handleChangeType = (type) => {
+    this.setState({
+      filters: {
+        type: type
+      }
+    })
+  }
+
+  handleFindPetsClick = () => {
+    let filterURL = "/api/"
+    let filterURLExtension;
+    if (this.state.filters.type === 'all') {
+      filterURLExtension = "pets"
+    } else if (this.state.filters.type === 'cat') {
+      filterURLExtension = "pets?type=cat"
+    } else if (this.state.filters.type === 'dog') {
+      filterURLExtension = "pets?type=dog"
+    } else if (this.state.filters.type === 'micropig') {
+      filterURLExtension = "pets?type=micropig"
+    }
+
+    fetch(filterURL + filterURLExtension)
+    .then(results => {
+      return results.json()
+    })
+    .then(json => {
+      let pets = []
+      json.forEach((pet) => {
+        pets.push(pet)
+      })
+      this.setState({pets: pets})
+    })
+    .catch(console.log)
+  }
+
+  handleAdoptPetClick = (id) => {
+    const pet = this.state.pets.find((pet) => {
+      return pet.id === id;
+    })
+    pet.isAdopted = true
+    this.setState({
+      pets: this.state.pets
+      
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +70,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+                onChangeType={this.handleChangeType}
+                onFindPetsClick={this.handleFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser 
+                pets={this.state.pets}
+                onAdoptPet={this.handleAdoptPetClick}
+              />
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,26 @@
 import React from 'react'
 
 class Filters extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  handleSubmit = () => {
+    console.log('fired handleSubmit')
+    this.props.onFindPetsClick()
+  }
+
+  handleChoice = (ev) => {
+    console.log('fired handleChoice')
+    this.props.onChangeType(ev.target.value)
+  }
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.handleChoice}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +29,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.handleSubmit}>Find pets</button>
         </div>
       </div>
     )
